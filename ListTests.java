5683d21c04b7e72cca4939f76d6473c2a2c40c16
ArihantJ <arihantjaingoa@gmail.com>
2023-01-26 11:49:10 -0800

    .

---
+++ ListTests.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import java.util.List;
+
+public class ListTests {
+    @Test
+    public void filtertest() {
+        //List<String> input1 = new List<String>;
+        //assertArrayEquals(new List<String> {"how", "are", "you"}, ListExamples.filter(input1));
+    }
+}
